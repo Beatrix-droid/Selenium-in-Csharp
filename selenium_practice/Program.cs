cb0bb10981caeb74255bd66888884866962a28f8
Beatrice <beatrice.federici123@gmail.com>
2023-03-07 10:57:12 +0000

    initial commit with driver setup

---
+++ selenium_practice/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Threading;
+
+namespace SeleniumFirefox;
+
+class program{
+
+IWebDriver driver;
+
+
+
+public void start_Browser(){
+    driver = new FirefoxDriver();
+    driver.Manage().Window.Maximize();
+
+}
+
+  public void quit_Browser(){
+    driver.Quit();
+  }
+}
+
